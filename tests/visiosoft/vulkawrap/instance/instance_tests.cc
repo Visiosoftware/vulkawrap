6304eb23ca69ce9675e85efe3761ef3797fc7a9c
robjclucas@gmail.com <robclu1818@gmail.com>
2016-03-11 15:31:12 +0200

    Created testing structure

---
+++ tests/visiosoft/vulkawrap/instance/instance_tests.cc
@@ -0,0 +1,21 @@
+//---- tests/visiosoft/vulkawrap/instance/instance_tests.cc  -*- C++ -*- ----//
+//---- Tests for the VUlkaWrap instance functionality.                   ----//
+//
+//                           Visiosoft Vulkawrap
+//                          
+//                      Copyright (c) 2016 Rob Clucas        
+//                    Distributed under the MIT License
+//                (See accompanying file LICENSE or copy at
+//                    https://opensource.org/licenses/MIT)
+//
+// ========================================================================= //
+//
+/// \file  instance_tests.cc
+/// \brief Implementation of the tests for the instance functionality of 
+///        VulkaWrap.
+//
+//---------------------------------------------------------------------------//
+
+#include "visiosoft/vulkawrap/instance/instance/h"
+
+// TODO: Add the tests ...
